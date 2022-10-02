fcbfdf3619bd72a10220ae24623173fb0ce3beb2
Jaron <jaronchretien@yahoo.com>
2022-10-02 17:40:40 -0500

    First Commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
+
+const root = ReactDOM.createRoot(document.getElementById('app'))
+
+const App = () => {
+
+    return (
+        <div>Hello World
+
+        </div>
+    )
+}
+
+
+root.render(<App />);
