c759f0d783334a417d7b7cfc3e09e00302eaec0c
Jaron <jaronchretien@yahoo.com>
2022-10-02 20:19:44 -0500

    Header Added (Missing Stuff)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-
-
+import React, {useState} from 'react'; //Enables JSX
+import ReactDOM from 'react-dom/client' //Allows us to Attach the App
 const root = ReactDOM.createRoot(document.getElementById('app'))
 
-const App = () => {
+const headerObj = {
+ textLinks: ["Home", "About", "Gear"]
+}
 
+const Header = ({props}) => {
     return (
-        <div>Hello World
-
-        </div>
+    <header>
+        <h1>CHON ALBUMS</h1>
+        <img className="chonLogo" src="https://d1h6olfj08dopz.cloudfront.net/images/41392/Chon_Logo_NavBar_Grow.png" alt=""/>
+        <nav className="wholenav">
+            {<a href="">{props}</a>}
+            
+        </nav>
+        
+    </header>
     )
 }
 
 
-root.render(<App />);
+
+
+//compenet is a function that returns HTML template
+const Main = () => {
+    return (
+        <main>
+            <Header props={headerObj.textLinks}/>
+        </main>
+         )
+}
+
+
+root.render(<Main />);
